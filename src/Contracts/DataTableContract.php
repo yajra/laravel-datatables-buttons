d383dd43665263668b420dec39b8695183fd6f04
Arjay Angeles <aqangeles@gmail.com>
2017-02-16 10:30:24 +0800

    Remove methods that are available on abstract class.

--- src/Contracts/DataTableContract.php
+++ src/Contracts/DataTableContract.php
@@ -11,16 +11,6 @@ namespace Yajra\Datatables\Contracts;
 interface DataTableContract
 {
     /**
-     * Render view.
-     *
-     * @param $view
-     * @param array $data
-     * @param array $mergeData
-     * @return \Illuminate\Http\JsonResponse|\Illuminate\View\View
-     */
-    public function render($view, $data = [], $mergeData = []);
-
-    /**
      * @return \Illuminate\Http\JsonResponse
      */
     public function ajax();
@@ -29,16 +19,6 @@ interface DataTableContract
      * @return \Yajra\Datatables\Html\Builder
      */
     public function html();
-
-    /**
-     * @return \Yajra\Datatables\Html\Builder
-     */
-    public function builder();
-
-    /**
-     * @return \Yajra\Datatables\Request
-     */
-    public function request();
 
     /**
      * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder|\Illuminate\Support\Collection
