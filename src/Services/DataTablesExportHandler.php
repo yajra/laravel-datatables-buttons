1f26308de105cdb79427a3124a0b0c38491a28b0
Arjay Angeles <aqangeles@gmail.com>
2018-03-29 02:27:49 +0800

    Include column headings.

--- src/Services/DataTablesExportHandler.php
+++ src/Services/DataTablesExportHandler.php
@@ -5,8 +5,9 @@ namespace Yajra\DataTables\Services;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class DataTablesExportHandler implements FromCollection
+class DataTablesExportHandler implements FromCollection, WithHeadings
 {
     use Exportable;
 
@@ -31,5 +32,18 @@ class DataTablesExportHandler implements FromCollection
     public function collection()
     {
         return $this->collection;
+    }
+
+    /**
+     * @return array
+     */
+    public function headings(): array
+    {
+        $first = $this->collection->first();
+        if ($first) {
+            return array_keys($first);
+        }
+
+        return [];
     }
 }
