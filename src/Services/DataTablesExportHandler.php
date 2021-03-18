8cd5dddc4cd421cb4e6271e571c42537d2ee27ba
Arjay Q. Angeles <aqangeles@gmail.com>
2021-03-18 18:25:14 +0800

    Fix imports.

--- src/Services/DataTablesExportHandler.php
+++ src/Services/DataTablesExportHandler.php
@@ -2,10 +2,6 @@
 
 namespace Yajra\DataTables\Services;
 
-use Illuminate\Support\Collection;
-use Maatwebsite\Excel\Concerns\Exportable;
-use Maatwebsite\Excel\Concerns\FromCollection;
-use Maatwebsite\Excel\Concerns\WithHeadings;
 use Yajra\DataTables\Exports\DataTablesCollectionExport;
 
 class DataTablesExportHandler extends DataTablesCollectionExport
