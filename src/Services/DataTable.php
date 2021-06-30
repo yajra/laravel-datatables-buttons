93de348771d49c31998923166b96c46ce2e7e5c4
Mário Valney <mariovalney@gmail.com>
2021-06-30 20:59:00 -0300

    Fix addScopes method pushing the array of scopes

--- src/Services/DataTable.php
+++ src/Services/DataTable.php
@@ -550,7 +550,7 @@ abstract class DataTable implements DataTableButtons
      */
     public function addScopes(array $scopes)
     {
-        array_push($this->scopes, $scopes);
+        array_merge($this->scopes, $scopes);
 
         return $this;
     }
