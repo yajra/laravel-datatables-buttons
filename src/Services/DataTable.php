9d053db190c92943f585effb0d361e4be17a5f68
Arjay Angeles <aqangeles@gmail.com>
2023-02-28 13:34:20 +0800

    fix: integration error message

--- src/Services/DataTable.php
+++ src/Services/DataTable.php
@@ -489,7 +489,7 @@ abstract class DataTable implements DataTableButtons
         }
 
         if (! class_exists(ExcelServiceProvider::class)) {
-            throw new Exception('Please composer require maatwebsite/excel to be able to use this function.');
+            throw new Exception('Please `composer require maatwebsite/excel` to be able to use this function.');
         }
 
         if (! new $this->exportClass instanceof DataTablesExportHandler) {
@@ -634,7 +634,7 @@ abstract class DataTable implements DataTableButtons
     public function snappyPdf(): Response
     {
         if (! class_exists(PdfWrapper::class)) {
-            throw new Exception('You need to install barryvdh/laravel-snappy to be able to use this feature.');
+            throw new Exception('Please `composer require barryvdh/laravel-snappy` to be able to use this feature.');
         }
 
         /** @var \Barryvdh\Snappy\PdfWrapper $snappy */
@@ -788,7 +788,7 @@ abstract class DataTable implements DataTableButtons
     protected function buildFastExcelFile(): FastExcel
     {
         if (! class_exists(FastExcel::class)) {
-            throw new Exception('Please composer require rap2hpoutre/fast-excel to be able to use this function.');
+            throw new Exception('Please `composer require rap2hpoutre/fast-excel` to be able to use this function.');
         }
 
         $query = null;
