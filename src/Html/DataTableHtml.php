908d1dbabfb29a0adeefcb81162ff7cf21f74c87
Arjay Angeles <aqangeles@gmail.com>
2026-03-26 20:33:28 +0800

    Revert "fix: use minifiedAjax instead of postAjax"

--- src/Html/DataTableHtml.php
+++ src/Html/DataTableHtml.php
@@ -46,7 +46,7 @@ abstract class DataTableHtml implements DataTableHtmlBuilder
         $this->htmlBuilder = app(Builder::class);
 
         $this->htmlBuilder
-            ->minifiedAjax($this->ajax())
+            ->postAjax($this->ajax())
             ->setTableId($this->tableId)
             ->selectSelector()
             ->selectStyleOs()
